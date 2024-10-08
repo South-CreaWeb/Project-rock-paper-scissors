e6ee039c109c09ed88e41ba999969ef0e0a420c4
South-CreaWeb <south-creaweb@outlook.fr>
2024-10-08 16:37:40 +0200

    Improve logic to play 5 round

--- js/app.js
+++ js/app.js
@@ -1,8 +1,5 @@
-let humainScore = 0
-let computerScore = 0 
-
 /**
- * 
+ * Create a random choice for computer
  * @returns {String}
  */
 function getComputerChoice(){
@@ -11,13 +8,13 @@ function getComputerChoice(){
 
     if(computerAnswer === 2) {
         computerAnswer = 'rock'
-        console.log(computerAnswer, 'rock')
+        console.log('rock')
     } else if(computerAnswer === 1){
         computerAnswer = 'paper'
-        console.log(computerAnswer, 'paper')
+        console.log('paper')
     }else{
         computerAnswer = 'scissors'
-        console.log(computerAnswer, 'scissors')
+        console.log('scissors')
     }
 
     return computerAnswer
@@ -27,34 +24,66 @@ let responseComputer = getComputerChoice()
 
 
 /**
- * 
+ * Create a response for the player 
  * @param {String} response 
  * @returns {String}
  */
 function getHumanChoice(){
 
-     let response = prompt(`Entrez l'une de ces trois valeurs: Rock, Paper or Scissors`).toLowerCase()
+    let response = prompt(`Entrez l'une de ces trois valeurs: Rock, Paper or Scissors`).toLowerCase()
 
     return response
 }
 
 let responseHuman = getHumanChoice()
 
+
 /**
- * 
- * @param {String} responseHuman 
- * @param {String} responseComputer 
+ * Create a game with 5 round
  */
-function playRound(responseHuman, responseComputer) {
+function playGame() {
 
-    if(responseHuman != responseComputer) {
+        
+    let humainScore = 0
+    let computerScore = 0
 
-        console.log('Gagner', `response humaine: ${responseHuman}, reponse ordi: ${responseComputer}`)
-    } else {
+    /**
+    * Create the logic for playing
+    * @param {String} responseHuman 
+    * @param {String} responseComputer 
+    */
+    function playRound(responseHuman, responseComputer) {
 
-        console.log('Perdu')
+        
+        if(responseHuman === "rock" && responseComputer === "scissors" || responseHuman === "scissors" && responseComputer === "paper" || responseHuman === 'paper' && responseComputer === "rock") {
+            humainScore++
+            console.log(`Vous avez gagné: ${responseHuman} bat ${responseComputer}, votre score est de: ${humainScore}`)
+        } else if (responseHuman === responseComputer){
+            console.log(`${responseHuman}, ${responseComputer}: égalité !`)
+        } else {
+            computerScore++
+            console.log(`L'ordinateur a gagner: le score ordinateur est de: ${computerScore}`)
+        }
+
+        getComputerChoice()
+        getHumanChoice()
+
     }
+    
+    playRound(responseHuman, responseComputer)
+    playRound(responseHuman, responseComputer)
+    playRound(responseHuman, responseComputer)
+    playRound(responseHuman, responseComputer)
+    playRound(responseHuman, responseComputer)
 
+    if(humainScore > computerScore) {
+        console.log(`Bravo vous avez gagner ! Avec un score de ${humainScore}`)
+    } else if(humainScore < computerScore) {
+        console.log('Vous avez perdu !')
+    } else {
+        console.log('Égalité !')
+    }
+    
 }
 
-playRound(responseHuman, responseComputer)
+playGame()
