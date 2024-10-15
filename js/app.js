063e03b4b70b51a941c1c2b0b9f9e98f21967122
South-CreaWeb <south-creaweb@outlook.fr>
2024-10-15 09:41:35 +0200

    Improve logic again

--- js/app.js
+++ js/app.js
@@ -20,22 +20,25 @@ function getComputerChoice(){
     return computerAnswer;
 }
 
-let responseComputer = getComputerChoice();
-
 
 /**
  * Create a response for the player 
- * @param {String} response 
  * @returns {String}
  */
 function getHumanChoice(){
 
     let response = prompt(`Entrez l'une de ces trois valeurs: Rock, Paper or Scissors`).toLowerCase();
 
-    return response;
-};
+    if(response === null) {
 
-let responseHuman = getHumanChoice();
+        response = prompt(`Entrez l'une de ces trois valeurs: Rock, Paper or Scissors`).toLowerCase();
+    } else {
+
+        return response;
+    }
+
+    
+};
 
 
 /**
@@ -49,40 +52,48 @@ function playGame() {
 
     /**
     * Create the logic for playing
-    * @param {String} responseHuman 
-    * @param {String} responseComputer 
+    * 
     */
-    function playRound(responseHuman, responseComputer) {
+    function playRound() {
+
+        let responseHuman = getHumanChoice();
+        let responseComputer = getComputerChoice();
+        console.log(responseComputer)
 
         
-        if(responseHuman === "rock" && responseComputer === "scissors" || responseHuman === "scissors" && responseComputer === "paper" || responseHuman === 'paper' && responseComputer === "rock") {
-            humainScore++;
-            console.log(`Vous avez gagné: ${responseHuman} bat ${responseComputer}, votre score est de: ${humainScore}`);
-        } else if (responseHuman === responseComputer){
-            console.log(`${responseHuman}, ${responseComputer}: égalité !`);
-        } else {
-            computerScore++;
-            console.log(`L'ordinateur a gagner: le score ordinateur est de: ${computerScore}`);
-        };
 
-        getComputerChoice();
-        getHumanChoice();
+        const humanPattern = (responseHuman === 'rock' && responseComputer === 'scissors') || (responseHuman === 'scissors' && responseComputer === 'paper') || (responseHuman === 'paper' && responseComputer === 'rock')
+        
+       if(humanPattern) {
+        console.log(responseHuman, responseComputer)
+        humainScore++
+        console.log(`Vous avez marqué ${humainScore} points !`)
+       } else if(responseHuman == responseComputer) {
+            console.log(`${responseHuman} = ${responseComputer} : Égalité !!`)
+       } else {
+        console.log(responseHuman, responseComputer)
+        computerScore++
+        console.log(`L'ordinateur a marqué ${computerScore} point`)
+       }
 
     };
+
+    playRound();
+    playRound();
+    playRound();
+    playRound();
+    playRound();
     
-    playRound(responseHuman, responseComputer);
-    playRound(responseHuman, responseComputer);
-    playRound(responseHuman, responseComputer);
-    playRound(responseHuman, responseComputer);
-    playRound(responseHuman, responseComputer);
 
     if(humainScore > computerScore) {
-        console.log(`Bravo vous avez gagner ! Avec un score de ${humainScore}`);
+        console.log(`Bravo vous avez gagner ! Avec un score de ${humainScore} contre ${computerScore}`);
     } else if(humainScore < computerScore) {
-        console.log('Vous avez perdu !');
+        console.log(`Vous avez perdu ! Résultat: ${computerScore} contre ${humainScore}`);
     } else {
-        console.log('Égalité !');
+        console.log(`Égalité ! Le score est de: ${humainScore} contre ${computerScore}`);
     };
+
+    
     
 };
 
