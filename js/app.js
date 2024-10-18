bfe64ae80cf27aced808ab5e89d5d5bd033fddaf
South-CreaWeb <south-creaweb@outlook.fr>
2024-10-18 16:53:36 +0200

    reorganize code

--- js/app.js
+++ js/app.js
@@ -2,115 +2,111 @@
  * Create a random choice for computer
  * @returns {String}
  */
-function getComputerChoice(){
+function getComputerChoice() {
+  let computerAnswer = Math.floor(Math.random() * 3);
 
-    let computerAnswer = Math.floor(Math.random() * 3);
+  if (computerAnswer === 2) {
+    computerAnswer = "rock";
+    console.log("rock");
+  } else if (computerAnswer === 1) {
+    computerAnswer = "paper";
+    console.log("paper");
+  } else {
+    computerAnswer = "scissors";
+    console.log("scissors");
+  }
 
-    if(computerAnswer === 2) {
-        computerAnswer = 'rock';
-        console.log('rock');
-    } else if(computerAnswer === 1){
-        computerAnswer = 'paper';
-        console.log('paper');
-    }else{
-        computerAnswer = 'scissors';
-        console.log('scissors');
-    };
-
-    return computerAnswer;
+  return computerAnswer;
 }
 
-
 /**
- * 
- * @param {HTMLElement} tagname 
+ *
+ * @param {HTMLElement} tagname
  * @returns {HTMLElement}
  */
 function createElement(tagname) {
+  let element = document.createElement(tagname);
 
-    let element = document.createElement(tagname)
-
-    return element
+  return element;
 }
 
 /**
  * Create a game
  */
 function playGame() {
+  let humainScore = 0;
+  let computerScore = 0;
+  let totalPointWin = 0;
 
-        
-    let humainScore = 0;
-    let computerScore = 0;
+  /**
+   * Create the logic for playing
+   *
+   */
+  function playRound() {
+    // DISPLAY GAME SCORE
 
-    /**
-    * Create the logic for playing
-    * 
-    */
-    function playRound() {
+    // Display score player
+    const divHumanScore = document.querySelector(".human-score");
+    const displayHumanScore = createElement("p");
+    displayHumanScore.innerText = `Votre score : ${humainScore}`;
 
-        
-        let responseComputer = getComputerChoice();
+    // Display score computer
+    const divComputerScore = document.querySelector(".computer-score");
+    const displayComputerScore = createElement("p");
+    displayComputerScore.innerText = `Ordinateur score: ${computerScore}`;
 
-        const humanPattern = (response === 'rock' && responseComputer === 'scissors') || (response === 'scissors' && responseComputer === 'paper') || (response === 'paper' && responseComputer === 'rock')
-        
-       if(humanPattern) {
-        console.log(responseHuman, responseComputer)
-        humainScore++
-        console.log(`Vous avez marqué ${humainScore} points !`)
-       } else if(responseHuman == responseComputer) {
-            console.log(`${responseHuman} = ${responseComputer} : Égalité !!`)
-       } else {
-        console.log(responseHuman, responseComputer)
-        computerScore++
-        console.log(`L'ordinateur a marqué ${computerScore} point`)
-       }
-
-       
-    };
+    setTimeout(() => {
+      divHumanScore.classList.add("active");
+      divHumanScore.appendChild(displayHumanScore);
+      divComputerScore.classList.add("active");
+      divComputerScore.appendChild(displayComputerScore);
+    }, 500);
 
     // CREATE SELECTOR FOR PLAYER
+    const divs = document.querySelectorAll("div");
+    const humanChoices = [divs[4], divs[5], divs[6]];
 
-        const divs = document.querySelectorAll('div')
+    for (choice of humanChoices) {
+      choice.addEventListener("click", (e) => {
+        let response = e.currentTarget.getAttribute("value");
+        console.log(response);
 
-        const humanChoices = [divs[4], divs[5], divs[6]]
+        let responseComputer = getComputerChoice();
 
-        for(choice of humanChoices) {
-            choice.addEventListener('click', (e) => {
-                let response = e.currentTarget.getAttribute('value')
-                console.log(response)
-                playRound()
-            })
+        const humanPattern =
+          (response === "rock" && responseComputer === "scissors") ||
+          (response === "scissors" && responseComputer === "paper") ||
+          (response === "paper" && responseComputer === "rock");
 
+        if (humanPattern) {
+          console.log(response, responseComputer);
+          humainScore++;
+          console.log(`Vous avez marqué ${humainScore} points !`);
+          displayHumanScore.innerText = `Votre score : ${humainScore}`;
+        } else if (response === responseComputer) {
+          console.log(`${response} = ${responseComputer} : Égalité !!`);
+        } else {
+          console.log(response, responseComputer);
+          computerScore++;
+          console.log(`L'ordinateur a marqué ${computerScore} point`);
+          displayComputerScore.innerText = `Ordinateur score: ${computerScore}`;
         }
 
+        totalPointWin++;
+        console.log(totalPointWin);
+        if (totalPointWin === 5) {
+          if (humainScore > computerScore) {
+            console.log("vous avez gagné");
+          } else if (humainScore < computerScore) {
+            console.log("ordi gagné");
+          } else {
+            console.log("Égalité");
+          }
+        }
+      });
+    }
+  }
 
-     
-    // DISPLAY GAME SCORE
-    const divHumanScore = document.querySelector('.human-score')
-    divHumanScore.classList.add('active')
-    const displayHumanScore = createElement('p')
-    displayHumanScore.innerText = `Votre score : ${humainScore}`
-
-    divHumanScore.appendChild(displayHumanScore)
-
-    const divComputerScore = document.querySelector('.computer-score')
-    divComputerScore.classList.add('active')
-    const displayComputerScore = createElement('p')
-    displayComputerScore.innerText = `Ordinateur score: ${computerScore}`
-
-    divComputerScore.appendChild(displayComputerScore)
-    
-
-
-    if(humainScore > computerScore) {
-        console.log(`Bravo vous avez gagner ! Avec un score de ${humainScore} contre ${computerScore}`);
-    } else if(humainScore < computerScore) {
-        console.log(`Vous avez perdu ! Résultat: ${computerScore} contre ${humainScore}`);
-    } else {
-        console.log(`Égalité ! Le score est de: ${humainScore} contre ${computerScore}`);
-    };
-
-    
-    
-};
+  playRound();
+}
 playGame();
